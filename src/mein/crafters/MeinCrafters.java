03ba7410b6ab40ebd1ded987829634be70747fe4
Michael Ortiz <ortiz.d.michael@gmail.com>
2015-04-24 15:39:12 -0700

    intial commit. All the stuff for a netbeans project including the needed libraries.

---
+++ src/mein/crafters/MeinCrafters.java
@@ -0,0 +1,13 @@
+package mein.crafters;
+
+/**
+ *
+ * @author michael
+ */
+public class MeinCrafters {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
