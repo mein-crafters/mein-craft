f328630143bbace2f43ad48281384e4bf5efefff
Michael Ortiz <ortiz.d.michael@gmail.com>
2015-05-01 12:33:23 -0700

    Added the start of the render function for rendering a cube

--- src/mein/crafters/MeinCrafters.java
+++ src/mein/crafters/MeinCrafters.java
@@ -10,4 +10,16 @@ public class MeinCrafters {
         // TODO code application logic here
     }
     
+    private void render(){
+        try{
+            glBegin(GL_QUADS);
+            //Top
+            glColor3f();
+            glVertex3f();
+            
+            //Bottom
+            
+        }
+    }
+    
 }
