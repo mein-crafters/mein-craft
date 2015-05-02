17078e6ad6484f6809a72cdaff3cdba7822c6916
Michael Ortiz <ortiz.d.michael@gmail.com>
2015-05-01 17:59:57 -0700

    Going off slides. Added more to Basic3D class

--- src/mein/crafters/Basic3D.java
+++ src/mein/crafters/Basic3D.java
@@ -5,14 +5,61 @@
  */
 package mein.crafters;
 
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import static org.lwjgl.opengl.GL11.GL_MODELVIEW;
+import static org.lwjgl.opengl.GL11.GL_NICEST;
+import static org.lwjgl.opengl.GL11.GL_PERSPECTIVE_CORRECTION_HINT;
+import static org.lwjgl.opengl.GL11.GL_PROJECTION;
+import static org.lwjgl.opengl.GL11.glClearColor;
+import static org.lwjgl.opengl.GL11.glHint;
+import static org.lwjgl.opengl.GL11.glLoadIdentity;
+import static org.lwjgl.opengl.GL11.glMatrixMode;
+import org.lwjgl.util.glu.GLU;
+
 /**
  *
  * @author michael
  */
 class Basic3D {
 
-    void start() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private FPCameraController fp = new FPCameraController(0f, 0f, 0f);
+    private DisplayMode displayMode;
+
+    public void start() {
+        try {
+            createWindow();
+            initGL();
+            fp.gameLoop();//render();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
-    
+
+    private void initGL() {
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        GLU.gluPerspective(100.0f, (float) displayMode.getWidth() / (float) displayMode.getHeight(), 0.1f, 300.0f);
+        glMatrixMode(GL_MODELVIEW);
+        glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+    }
+
+    private void createWindow() throws Exception {
+        Display.setFullscreen(false);
+        DisplayMode d[]
+                = Display.getAvailableDisplayModes();
+        for (int i = 0; i < d.length; i++) {
+            if (d[i].getWidth() == 640
+                    && d[i].getHeight() == 480
+                    && d[i].getBitsPerPixel() == 32) {
+                displayMode = d[i];
+                break;
+            }
+        }
+        Display.setDisplayMode(displayMode);
+        Display.setTitle("Mein-Crafter!");
+        Display.create();
+    }
 }
