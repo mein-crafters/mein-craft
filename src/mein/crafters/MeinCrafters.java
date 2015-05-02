5fd40f25b6eba378d302e232c39a736c8a85b8da
Michael Ortiz <ortiz.d.michael@gmail.com>
2015-05-01 17:28:05 -0700

    Copy pasted some general info. We need a class, so i created that and need to do more in render function

--- src/mein/crafters/MeinCrafters.java
+++ src/mein/crafters/MeinCrafters.java
@@ -1,5 +1,11 @@
 package mein.crafters;
 
+import static org.lwjgl.opengl.GL11.GL_QUADS;
+import static org.lwjgl.opengl.GL11.glBegin;
+import static org.lwjgl.opengl.GL11.glColor3f;
+import static org.lwjgl.opengl.GL11.glEnd;
+import static org.lwjgl.opengl.GL11.glVertex3f;
+
 /**
  *
  * @author michael
@@ -7,19 +13,30 @@ package mein.crafters;
 public class MeinCrafters {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        Basic3D basic = new Basic3D();
+        basic.start();
     }
-    
-    private void render(){
-        try{
+
+    private void render() {
+        try {
             glBegin(GL_QUADS);
             //Top
-            glColor3f();
-            glVertex3f();
-            
+            glColor3f(1.0f, 0.0f, 1.0f);
+            glVertex3f(1.0f, -1.0f, -1.0f);
+            glVertex3f(-1.0f, -1.0f, -1.0f);
+            glVertex3f(-1.0f, 1.0f, -1.0f);
+            glVertex3f(1.0f, 1.0f, -1.0f);
             //Bottom
-            
+            glColor3f(0.0f, 1.0f, 1.0f);
+            glVertex3f(1.0f, -1.0f, -1.0f);
+            glVertex3f(-1.0f, -1.0f, -1.0f);
+            glVertex3f(-1.0f, 1.0f, -1.0f);
+            glVertex3f(1.0f, 1.0f, -1.0f);
+            glEnd();
+            //
+        } catch (Exception e) {
+
         }
     }
-    
+
 }
